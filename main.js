34da8c7aa1d38a29e82be4161e159b74332e7c09
tugaydmrl <contact@tugaydemirel.com>
2025-10-29 19:30:42 +0300

    feat: implement preload script and update main window configuration

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import { app, BrowserWindow } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
   mainWindow.loadFile("index.html");
 };
